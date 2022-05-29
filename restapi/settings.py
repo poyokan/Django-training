e9e57ca9c4969e813b2a01e86673503301a1a92f
MatsuKazu <kanzaki-trp@outlook.jp>
2022-05-29 03:42:16 +0000

    settings.pyにmysqlの設定追加

--- restapi/settings.py
+++ restapi/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'restapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'db_mysql',
+        "USER": "root",
+        "PASSWORD": "password"
     }
 }
 
